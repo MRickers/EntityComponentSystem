fc6573e624d33945d7e1033267c18ee098c56e5b
Ri <max_rickers@hotmail.de>
2021-09-25 20:14:10 +0200

    RenderSystem interface first draft

---
+++ include/ecs/systems/render_system.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <memory>
+
+#include "ecs/core/system_manager.h"
+
+namespace ecs {
+	namespace system {
+		class RenderSystem : public ecs::core::System {
+		private:
+
+		public:
+			virtual ~RenderSystem() {}
+			virtual void Update(uint32_t dt) = 0;
+
+			virtual void Init() {}
+		};
+
+		enum class Renderlib : int {
+			Sdl2,
+			SFML,
+		};
+
+		class RenderSystemFactory {
+		public:
+			std::unique_ptr<RenderSystem> MakeSystem(const Renderlib renderlib)  const;
+		};
+	}
+}
