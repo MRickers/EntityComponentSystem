b7acac3f8cf6ddf4f9e8621958f06dfe1a9bfb32
Max <max_rickers@hotmail.de>
2021-12-27 15:14:26 +0100

    Added timer fps example

--- src/examples/timer_example.cpp
+++ src/examples/timer_example.cpp
@@ -9,5 +9,21 @@ int main() {
 	ecs::utils::Timer timer;
 	std::this_thread::sleep_for(1000ms);
 	lLog(lInfo) << "Sleept for " << timer.GetTime() << "ms";
+
+	float fps = 0;
+	int count = 0;
+	timer.Start();
+	while (true) {
+		if (const auto dt = timer.GetTime(); dt >= 16) {
+			timer.Start();
+			fps += dt;
+			count++;
+			if (fps >= 1000) {
+				fps /= count*1000;
+				lLog(lDebug) << "FPS: " << 1/fps;
+				count = 0;
+			}
+		}
+	}
 	return 0;
 }
